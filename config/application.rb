b1bdbd45f35571071b43e7fc58f7b130d3b7d8f2
thuylinh <thuylinhrndm@gmail.com>
2016-03-03 16:18:22 +0000

    css

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module DublinTour
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.sass.load_paths += [
+  "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/stylesheets/foundation/components",
+  "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/stylesheets/foundation/"
+]
   end
 end
