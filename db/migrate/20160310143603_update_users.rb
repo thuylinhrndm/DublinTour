1efa262310faec184dbcd8b1a93fd6888207d996
thuylinh <thuylinhrndm@gmail.com>
2016-03-20 18:34:36 +0000

    added some user scaffold to inspect db

--- db/migrate/20160310143603_update_users.rb
+++ db/migrate/20160310143603_update_users.rb
@@ -1,6 +1,6 @@
 class UpdateUsers < ActiveRecord::Migration
   def change
     @u = User.find_by( email: 'admin@email.ie' )
-    @u.update_attribute :admin, true 
+    @u.update_attributes(admin: true)
   end
 end
